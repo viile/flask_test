35374427e5f68a36cb6613f462d69783585d797c
viile <dakgzhu@foxmail.com>
2015-05-11 10:07:33 +0800

    add codes

--- flaskr.py
+++ flaskr.py
@@ -5,8 +5,16 @@ from flask import Flask, g,current_app,session,request, session,redirect, url_fo
 import MySQLdb
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+	name = StringField('What is your name?',validators=[Required()])
+	submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'wtf come'
 bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
@@ -23,5 +31,18 @@ def cookie():
     response = make_response('carries a cookie')
     response.set_cookie('answer','42')
     return response
+@app.route('/name/',methods=['GET','POST'])
+def name():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        flash('i will like you ')
+        form.name.data = ''
+        return redirect(url_for('name'))
+    return render_template('name.html',form=form,name=session.get('name'))
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
 app.debug = True
 app.run(host='0.0.0.0')
